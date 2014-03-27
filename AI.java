d43db2c2a8e9ea26b74a1c4c992c7582faf23ef9
randomguy64 <64.randomguy@gmail.com>
2014-03-27 11:20:39 -0700

    renaming/archiving

--- AI.java
+++ AI.java
@@ -5,7 +5,7 @@ public class AI {
 	//Meta-Settings
 	public static final String VERSION = "1.2.2";
 	public static String name = "WerryJu";
-	public static int trials = 10;
+	public static int trials = 40;
 	public static boolean autoRestart = true;
 	public static boolean recording = true;
 	private static boolean thisAIIsCheating = false;
